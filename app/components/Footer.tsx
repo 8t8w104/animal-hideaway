6d25b791c94c7efabfab430c4d149ecd0d2adb04
Your Name <8t8w104@gmail.com>
2025-01-23 01:13:27 +0900

    ヘッダー、フッター作成

---
+++ app/components/Footer.tsx
@@ -0,0 +1,13 @@
+
+export const Footer = () => {
+  return (
+    <>
+      <footer style={{ backgroundColor: "pink" }}>
+        <div>フッター領域</div>
+        <p>
+          Powered by test
+        </p>
+      </footer>
+    </>
+  );
+}
