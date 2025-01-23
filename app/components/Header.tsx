9ebe3e1771a9adaec410def5847f77e7c3ae7f75
8t8w104 <8t8w104@gmail.com>
2025-01-23 10:27:30 +0900

    ヘッダーにサインアウト機能追加 (#17)
    * Googleボタンコンポーネント作成
    
    * Twitterボタンコンポーネント作成
    
    * Mantine UI導入
    
    * 個人/団体のログイン画面UI仮修正
    
    * 不要ファイル削除
    
    * signOut後のリダイレクト先修正
    
    * ヘッダーにサインアウト機能追加

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,40 +1,16 @@
-import Link from "next/link";
+import { createClient } from "@/utils/supabase/server";
+import { HeaderClient } from "./HeaderClient";
 
-export const Header = () => {
+export const Header = async () => {
+  const supabase = await createClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
   return (
-    <>
-      <header style={{ backgroundColor: 'lightblue' }}>
-        <div>ヘッダー領域</div>
-        <nav>
-          <ol>
-            <li>
-              <Link href="/login/organization">
-                団体のログイン画面
-              </Link>
-            </li>
-            <li>
-              <Link href="/login/personal">
-                個人のログイン画面
-              </Link>
-            </li>
-            <li>
-              <Link href="/regist">
-                動物を登録する
-              </Link>
-            </li>
-            <li>
-              <Link href="/animals">
-                動物を検索する
-              </Link>
-            </li>
-            <li>
-              <Link href="/contact">
-                運営に問い合わせる
-              </Link>
-            </li>
-          </ol>
-        </nav>
-      </header>
-    </>
+    <header>
+      <HeaderClient user={user} />
+    </header>
   );
-}
+};
---
+++ app/components/HeaderClient.tsx
@@ -0,0 +1,45 @@
+'use client'
+import Link from "next/link";
+import { signOutAction } from "../actions";
+import { User } from "@supabase/supabase-js";
+
+const navItems = [
+  { href: "/login/organization", label: "団体のログイン画面" },
+  { href: "/login/personal", label: "個人のログイン画面" },
+  { href: "/regist", label: "動物を登録する" },
+  { href: "/animals", label: "動物を検索する" },
+  { href: "/contact", label: "運営に問い合わせる" },
+];
+
+export const HeaderClient = ({ user }: { user: User | null }) => {
+  const handleSignOutClick = () => {
+    signOutAction()
+  };
+  return (
+    <>
+      <header style={{ backgroundColor: 'lightblue' }}>
+        <div>ヘッダー領域</div>
+        <nav>
+          <ol>
+            {navItems.map((item, index) => (
+              <li key={index}>
+                <Link href={item.href}>{item.label}</Link>
+              </li>
+            ))}
+
+            {user &&
+              <li>
+                <Link href="#" onClick={(e) => {
+                  e.preventDefault();
+                  handleSignOutClick();
+                }}>
+                  Sign out
+                </Link>
+              </li>
+            }
+          </ol>
+        </nav>
+      </header>
+    </>
+  );
+}
