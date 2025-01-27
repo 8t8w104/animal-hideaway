318a60ff0e7342e8c728fbf3c32d38ae8ed48b51
8t8w104 <8t8w104@gmail.com>
2025-01-27 22:11:28 +0900

    一覧画面をサーバコンポーネントとクライアントコンポーネントに分ける対応

--- app/animals/page.tsx
+++ app/animals/page.tsx
@@ -1,7 +1,10 @@
+
 import { Animal, Gender } from '@prisma/client';
 import { Animals } from './components/Animals';
-import { AspectRatio, Button, Card, Center, Container, Flex, Image, NumberInput, SimpleGrid, Stack, Text, TextInput, Title } from '@mantine/core';
-import classes from './ArticlesCardsGrid.module.css';
+import { redirect } from 'next/navigation';
+
+import { cookies } from 'next/headers';
+import { createClient } from '@/utils/supabase/server';
 
 // /* 以降、アプリのテーブル定義 */
 // enum Gender {
@@ -29,32 +32,6 @@ export type TodoType = {
   status: number;
   userId: string;
 }
-
-// const list = [
-//   {
-//     id: 1,
-//     title: "title1",
-//     content: "content1",
-//     status: 1,
-//     userId: "userId1",
-//   },
-//   {
-//     id: 2,
-//     title: "title2",
-//     content: "content2",
-//     status: 1,
-//     userId: "userId2",
-//   },
-// ] as Animal
-
-// const mockdata = [
-//   {
-//     title: 'Top 10 places to visit in Norway this summer',
-//     image:
-//       'https://images.unsplash.com/photo-1527004013197-933c4bb611b3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80',
-//     date: 'August 18, 2022',
-//   },
-// ];
 
 const data: Animal[] = [
   {
@@ -92,69 +69,40 @@ const data: Animal[] = [
 export default async function Page() {
 
 
+    const supabase = await createClient();
+  
+    // セッションを取得
+    const { data: { session } } = await supabase.auth.getSession();
+    console.log(session)
+    console.log("↑  session")
 
-  const cards = data.map((article) => (
-    <Card key={article.id} component="a" href="#" className={classes.card}>
-      <Text>
-        {article.name}
-      </Text>
-      <AspectRatio ratio={1920 / 1080} >
-        <Image className={classes["image-wrapper"]} src={article.description || "/assets/noimage.png"} alt="Default image" width={400} height={300} />
-      </AspectRatio>
-      <Text c="dimmed" size="xs" tt="uppercase" fw={700} mt="md">
-        {article.animalTypeId}
-      </Text>
-      <Text />
+    const { data: { user } } = await supabase.auth.getUser();
+  console.log(user)
+  console.log("↑  user")
 
-      <Text>
-        {article.gender}
-      </Text>
-      <Text>
-        {article.applicationStatus}
-      </Text>
-      <Text>
-        {article.publicStatus}
-      </Text>
+  if(!session) {
+    redirect('/login');
+  }
+  // ホスト情報を取得
+  const baseUrl = process.env.SUPABASE_URL || 'http://localhost:3000';
 
-    </Card>
-  ));
+  // TODO データベースから取得
+    const res = await fetch(`${baseUrl}/api/animals`, {
+      method: "GET",
+      cache: 'no-store',
+      headers: {
+        // 'Authorization': `Bearer ${user?.id}`,
+        'Authorization': `Bearer test`,
+      },
+    });
+    const data = await res.json();
+    console.log(data);
+    console.log("↑一覧取得しました。");
 
-
+  
   return (
     <>
-      {/* <Animals animals={list} /> */}
-
-      <Center>
-        <Stack>
-          <Title order={5} >検索条件</Title>
-          <Flex>
-            <TextInput
-
-              label="Name"
-              placeholder="Name"
-              bg="lightgreen"
-            />
-            <TextInput
-              bg="lightgreen"
-              label="Email"
-              placeholder="Email"
-
-            />
-            <NumberInput
-              label="Age"
-              placeholder="Age"
-              min={0}
-              max={99}
-            />
-            <Button>
-              Submit
-            </Button>
-          </Flex>
-        </Stack>
-      </Center>
-      <Container py="xl">
-        <SimpleGrid cols={{ base: 1, sm: 2 }}>{cards}</SimpleGrid>
-      </Container>
+      <Animals animals={data} />
     </>
   );
 }
