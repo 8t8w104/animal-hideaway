956b095a9109cf6c89d587489cf04ce32f9f8dd2
Your Name <8t8w104@gmail.com>
2025-02-02 01:02:33 +0900

    a

--- app/api/animals/[id]/route.ts
+++ app/api/animals/[id]/route.ts
@@ -1,42 +1,40 @@
 import { NextRequest, NextResponse } from "next/server";
-// import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 
-// const prisma = new PrismaClient();
+const prisma = new PrismaClient();
 
 export async function GET(req: NextRequest) {
+  // ルートパラメータの`id`を取得
+  const pathname = req.nextUrl.pathname;
+  const id = pathname.split('/').pop();
+
+  if (!id) {
+    return NextResponse.json({ error: "IDが指定されていません。" }, { status: 400 });
+  }
+
+  // Prismaで検索
+  const animal = await prisma.animal.findUnique({
+    where: { id: Number(id) },
+    select: {
+      id: true,
+      name: true,
+      description: true,
+      gender: true,
+      applicationStatus: true,
+      publicStatus: true,
+      animalType: {
+        select: {
+          id: true,
+          type: true
+        }
+      }
+    },
+  });
+
+  // 結果を返却
+  if (!animal) {
+    return NextResponse.json({ error: "動物が見つかりません。" }, { status: 404 });
+  }
+
+  return NextResponse.json(animal);
 }
-// export async function GET(req: NextRequest) {
-//   // ルートパラメータの`id`を取得
-//   const pathname = req.nextUrl.pathname;
-//   const id = pathname.split('/').pop();
-
-//   if (!id) {
-//     return NextResponse.json({ error: "IDが指定されていません。" }, { status: 400 });
-//   }
-
-//   // Prismaで検索
-//   const animal = await prisma.animal.findUnique({
-//     where: { id: Number(id) },
-//     select: {
-//       id: true,
-//       name: true,
-//       description: true,
-//       gender: true,
-//       applicationStatus: true,
-//       publicStatus: true,
-//       animalType: {
-//         select: {
-//           id: true,
-//           type: true
-//         }
-//       }
-//     },
-//   });
-
-//   // 結果を返却
-//   if (!animal) {
-//     return NextResponse.json({ error: "動物が見つかりません。" }, { status: 404 });
-//   }
-
-//   return NextResponse.json(animal);
-// }
