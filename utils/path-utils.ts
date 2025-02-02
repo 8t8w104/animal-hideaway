d72687ac927797dd06bc63e075516b680779dc4d
Your Name <8t8w104@gmail.com>
2025-02-02 21:32:32 +0900

    ファイルパス生成ルール変更

--- utils/path-utils.ts
+++ utils/path-utils.ts
@@ -3,15 +3,14 @@ import { generateUUID } from "./uuid";
 /**
  * ファイルパスを生成するメソッド
  * @param userId ユーザーID
- * @param animalId 動物ID
  * @returns 生成されたファイルパス
  */
-export const generateFilePath = ({ userId, animalId }: { userId: string, animalId: string }): string => {
+export const generateFilePath = ({ userId }: { userId: string }): string => {
   const uuid = generateUUID();
   const date = new Date();
   const formattedDate = `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, "0")}-${String(date.getDate()).padStart(2, "0")}`;
   const formattedTime = `${String(date.getHours()).padStart(2, "0")}${String(date.getMinutes()).padStart(2, "0")}${String(date.getSeconds()).padStart(2, "0")}`;
 
-  // /{ユーザーID}/{動物ID}/{ランダムな文字列}{日付}_{時間}
-  return `/${userId}/${animalId}/${uuid}/${formattedDate}_${formattedTime}`;
+  // /{ユーザーID}/{ランダムな文字列}{日付}_{時間}
+  return `/${userId}/${uuid}/${formattedDate}_${formattedTime}`;
 };
