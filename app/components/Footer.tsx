8cd495b3953bc97d10d04155533db04ad22dc558
Your Name <8t8w104@gmail.com>
2025-02-11 23:02:58 +0900

    フッターレイアウト修正

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,13 +1,35 @@
+'use client'
+import { Box, Text, useMantineTheme, Group, ActionIcon, Center } from '@mantine/core';
+import { IconBrandInstagram, IconBrandX } from '@tabler/icons-react'
 
 export const Footer = () => {
+  const theme = useMantineTheme();
+
+  const currentYear = new Date().getFullYear();
+
   return (
-    <>
-      <footer style={{ backgroundColor: "pink" }}>
-        <div>フッター領域</div>
-        <p>
-          Powered by test
-        </p>
-      </footer>
-    </>
+    <Box
+      style={{
+        backgroundColor: theme.colors.gray[0], // ヘッダーと同じ背景色
+        padding: theme.spacing.md,
+      }}
+    >
+      <Center>
+        <Group gap="xl"> {/* 中央揃え */}
+          <Text style={{ color: "dimmed" }} size="sm">
+            &copy; {currentYear} Animal Matching. All rights reserved.
+          </Text>
+          <Group gap="sm">
+            <ActionIcon disabled>
+              <IconBrandX size={18} />
+            </ActionIcon>
+            <ActionIcon disabled>
+              <IconBrandInstagram size={18} />
+            </ActionIcon>
+          </Group>
+
+        </Group>
+      </Center>
+    </Box>
   );
-}
+};
